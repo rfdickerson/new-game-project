ccefe3d83d6d86e0b40213c192a01f48cf98705b
Robert F. Dickerson <rfdickerson@gmail.com>
2025-11-22 18:14:37 -0600

    now labels are bold

--- left_panel.gd
+++ left_panel.gd
@@ -1,15 +1,15 @@
 extends Panel
 
 @onready var city_name_label: Label = $VBoxContainer/CityInfoContainer/CityNameLabel
-@onready var population_label: Label = $VBoxContainer/CityInfoContainer/PopulationLabel
-@onready var food_label: Label = $VBoxContainer/CityInfoContainer/FoodLabel
-@onready var production_label: Label = $VBoxContainer/CityInfoContainer/ProductionLabel
-@onready var gold_income_label: Label = $VBoxContainer/CityInfoContainer/GoldIncomeLabel
+@onready var population_value: Label = $VBoxContainer/CityInfoContainer/PopulationContainer/PopulationValue
+@onready var food_value: Label = $VBoxContainer/CityInfoContainer/FoodContainer/FoodValue
+@onready var production_value: Label = $VBoxContainer/CityInfoContainer/ProductionContainer/ProductionValue
+@onready var gold_income_value: Label = $VBoxContainer/CityInfoContainer/GoldIncomeContainer/GoldIncomeValue
 @onready var next_turn_button: Button = $VBoxContainer/NextTurnButton
 
 var game_ui: Control = null
 
-var city_name: String = "Alexandria"
+var city_name: String = "Nassau"
 var population: int = 1000
 var food: int = 50
 var production: int = 25
@@ -23,14 +23,14 @@ func _ready():
 func update_ui():
 	if city_name_label:
 		city_name_label.text = city_name
-	if population_label:
-		population_label.text = "Population: %d" % population
-	if food_label:
-		food_label.text = "Food: %d" % food
-	if production_label:
-		production_label.text = "Production: %d/turn" % production
-	if gold_income_label:
-		gold_income_label.text = "Gold Income: +%d/turn" % gold_income
+	if population_value:
+		population_value.text = "%d" % population
+	if food_value:
+		food_value.text = "%d" % food
+	if production_value:
+		production_value.text = "%d/turn" % production
+	if gold_income_value:
+		gold_income_value.text = "+%d/turn" % gold_income
 
 func set_game_ui(ui: Control):
 	game_ui = ui
@@ -58,4 +58,3 @@ func set_gold_income(income: int):
 func _on_next_turn_pressed():
 	if game_ui:
 		game_ui.next_turn()
-
